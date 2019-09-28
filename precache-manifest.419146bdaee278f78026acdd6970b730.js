899bf6cd25bff506377f2ea8661b9704a3508a00
Denise Tan <techdenisetan@gmail.com>
2019-09-28 14:48:05 +0800

    Updates

---
+++ precache-manifest.419146bdaee278f78026acdd6970b730.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a78d517828594e6d143b1b84a38d867d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7d96c3447970ecab67f5",
+    "url": "/monsters-rolodex/static/css/main.fe9a5e61.chunk.css"
+  },
+  {
+    "revision": "a635542e9a5f89fce9c0",
+    "url": "/monsters-rolodex/static/js/2.594a10ce.chunk.js"
+  },
+  {
+    "revision": "7d96c3447970ecab67f5",
+    "url": "/monsters-rolodex/static/js/main.246b7174.chunk.js"
+  },
+  {
+    "revision": "7548f6fbafc46e9f04b0",
+    "url": "/monsters-rolodex/static/js/runtime~main.4eb04847.js"
+  }
+]);
